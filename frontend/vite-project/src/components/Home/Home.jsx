c21b70033e568ccc231bc27a6faa122939a10153
MuhammedMuneeb <muneebvazhangattil6@gmail.com>
2023-10-28 19:14:38 +0530

    home page design started

--- frontend/vite-project/src/components/Home/Home.jsx
+++ frontend/vite-project/src/components/Home/Home.jsx
@@ -5,7 +5,23 @@ function Home() {
   return (
     <div>
     <Navbar/>
-    
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto p-4">
+        <header>
+          {/* Navbar can go here if you decide to add it */}
+          {/* <nav className="bg-blue-500 p-4">Navbar content</nav> */}
+        </header>
+
+        <main className="mt-8">
+          <h1 className="text-3xl font-bold text-gray-800">Welcome to the Reporting Website</h1>
+          <p className="text-gray-600 mt-4">Your reporting website content goes here.</p>
+        </main>
+      </div>
+
+      {/* Footer can go here if you decide to add it */}
+      {/* <footer className="bg-blue-500 p-4 text-center text-white">Footer content</footer> */}
+    </div>
+  
     </div>
   )
 }
